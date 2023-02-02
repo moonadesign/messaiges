d39cf3690494466947db090da23fb166d38d689a
Matt Born <mattborn@users.noreply.github.com>
2023-02-02 16:56:59 -0700

    Moved fetch to Cloud Function

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+}
--- scripts.js
+++ scripts.js
@@ -1,21 +1,14 @@
 const g = document.getElementById.bind(document)
-const q = document.querySelectorAll.bind(document)
 
 const message = (text) => {
+  console.log(text)
   g('text').innerText = ''
   g('typing').style.visibility = 'visible'
-  fetch('https://api.openai.com/v1/completions', {
+  fetch('https://us-central1-samantha-374622.cloudfunctions.net/messaiges', {
     method: 'POST',
-    headers: {
-      Authorization:
-        'Bearer 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',
-      'Content-Type': 'application/json',
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      max_tokens: 200,
-      model: 'text-davinci-003',
-      prompt: text,
-      temperature: 1,
+      text: text,
     }),
   })
     .then((response) => response.json())
@@ -24,7 +17,7 @@ const message = (text) => {
       render(data.choices[0].text, true)
     })
     .catch((error) => {
-      render(error)
+      render(error, true)
     })
 }
 
@@ -39,11 +32,14 @@ const send = () => {
   const text = g('text').innerText
   render(text)
   message(
-    `I'm going to say random things and ask questions and I want you to reply to me like we are having an informal conversation so don’t worry about perfect punctuation. Use slang sometimes. A few expletives are fine, too. The random thing or question is this: ${text}`,
+    `I'm going to say random things and ask questions and I want you to reply to me like we are having an informal conversation so don’t worry about perfect punctuation. Use slang sometimes. A few expletives are fine, too. The random thing or question is this: ${text}.`,
   )
 }
 
 const app = () => {
+  document.querySelector('body').style.overflow = 'hidden'
   g('app').style.display = 'block'
-  message('Greet me in a unique way and name yourself')
+  message(
+    'Greet me in a fun, silly, or surprising way and name yourself something clever',
+  )
 }
